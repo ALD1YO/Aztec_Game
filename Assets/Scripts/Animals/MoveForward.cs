a1920f4d025ae40d887b4e0083834db4fbfecf35
ald1yo <crespoaldo29@gmail.com>
2020-03-17 21:54:33 -0600

    Se mejora el rendimiento del escenario, se descubren múltiples terrenos

---
+++ Assets/Scripts/Animals/MoveForward.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float Velocidad=10;
+
+
+    void Update()
+    {
+
+        transform.Translate(0, 0, Velocidad);
+    }
+}
