13005723ef30425d644cab67825920b2a575feb2
JuanCasanova <jucasanovacr@ucq.edu.mx>
2020-04-08 21:59:39 -0500

    Actualizar branch del pajarito

---
+++ Assets/Scripts/PajaritoVolar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PajaritoVolar : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name== "sinsonte_idle")
+        {
+            Debug.Log("vas bien papu");
+        }
+    }
+}
