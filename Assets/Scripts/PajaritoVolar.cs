ef8eeeb63b981bc60daef901835ddbde4f64143a
JuanCasanova <jucasanovacr@ucq.edu.mx>
2020-04-24 16:15:57 -0500

    El pajarito ya vuela gracias a que se implemento lerp

--- Assets/Scripts/PajaritoVolar.cs
+++ Assets/Scripts/PajaritoVolar.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 
 public class PajaritoVolar : MonoBehaviour
 {
-    bool Vuela;
+    bool Activo;
+
     float Posx;
     float Posy;
+    float t1;
+    float t2;
 
     private void Start()
     {
-        Vuela = false;   
+        Activo = false;
     }
 
     private void Update()
     {
-        if(Vuela==true)
+        if(Activo)
         {
-            this.transform.position = new Vector3(Posx, Posy, this.transform.position.z);
+            t1 += .4f * Time.deltaTime;
+            t2 += .1f * Time.deltaTime;
+            this.transform.position = new Vector3(Mathf.Lerp(Posx, Posx + 20, t1), Mathf.Lerp(Posy, Posy + 20, t2), this.transform.position.z);
         }
     }
 
@@ -28,23 +33,52 @@ public class PajaritoVolar : MonoBehaviour
         {
             Posx = this.transform.position.x;
             Posy = this.transform.position.y;
-            Vuela = true;
-            Volar();
-        }
-    }
-
-    void Volar()
-    {
-        Debug.Log(Posx + ","+Posy);
-        for(float i=0; i<=1000; i+=.01f)
-        {
-            Posx += .0001f;
-            Posy += .0001f;
-            if(i>=1000)
-            {
-                Vuela = false;
-                Debug.Log(Vuela);
-            }
+            Activo = true;
         }
     }
 }
+
+
+/*
+bool Vuela;
+float Posx;
+float Posy;
+
+private void Start()
+{
+    Vuela = false;
+}
+
+private void Update()
+{
+    if (Vuela == true)
+    {
+        this.transform.position = new Vector3(Posx, Posy, this.transform.position.z);
+    }
+}
+
+void OnTriggerEnter(Collider collision)
+{
+    if (collision.gameObject.tag == "Player")
+    {
+        Posx = this.transform.position.x;
+        Posy = this.transform.position.y;
+        Vuela = true;
+        Volar();
+    }
+}
+
+void Volar()
+{
+    Debug.Log(Posx + "," + Posy);
+    for (float i = 0; i <= 1000; i += .01f)
+    {
+        Posx += .0001f;
+        Posy += .0001f;
+        if (i >= 1000)
+        {
+            Vuela = false;
+            Debug.Log(Vuela);
+        }
+    }
+}*/
