7253f956adb34e33f96a72d8c23041f06052c0b8
ald1yo <crespoaldo29@gmail.com>
2020-05-11 13:19:53 -0500

    Bambi ya hace ragdoll cuando toca al jugador, sin embargo, no reacciona directamente al hitbox de los golpes

---
+++ Assets/Scripts/Animals/RagDollToggle.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagDollToggle : MonoBehaviour
+{
+    protected Animator Animator;
+    protected Rigidbody Rigidbody;
+    protected BoxCollider BoxCollider;
+    protected MoveForward MoveForward;
+
+    protected Collider[] ChildrenCollider;
+    protected Rigidbody[] ChildrenRigidBody;
+
+
+    void Awake()
+    {
+        Animator = GetComponent<Animator>();
+        Rigidbody = GetComponent<Rigidbody>();
+        BoxCollider = GetComponent<BoxCollider>();
+        MoveForward = GetComponent<MoveForward>();
+
+        ChildrenCollider = GetComponentsInChildren<Collider>();
+        ChildrenRigidBody = GetComponentsInChildren<Rigidbody>();
+    }
+
+    void Start()
+    {
+        RagdollActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void RagdollActive(bool active)
+    {
+        //children
+        foreach (var collider in ChildrenCollider)
+            collider.enabled = active;
+        foreach(var rigidbody in ChildrenRigidBody)
+        {
+            rigidbody.detectCollisions = active;
+            rigidbody.isKinematic = !active;
+        }
+
+        //root
+        Animator.enabled = !active;
+        Rigidbody.detectCollisions = !active;
+        Rigidbody.isKinematic = active;
+        BoxCollider.enabled = !active;
+        MoveForward.enabled = !active;
+
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.other.GetComponent<Combat_Trigger>() != null)
+        {
+            RagdollActive(true);
+        }
+
+        if (collision.other.gameObject.tag == "Hit")
+        {
+            //RagdollActive(true);
+        }
+
+        if(collision.other.gameObject.name == "Hitbox")
+        {
+            //RagdollActive(true);
+        }
+    }
+}
