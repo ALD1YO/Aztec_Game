ce26f82a2e5bd5437e6c3e4ca0721758476a8bf7
JuanCasanova <jucasanovacr@ucq.edu.mx>
2020-05-11 08:53:20 -0500

    El pajaro ya vuela, bambi no se hunde, pero falta que corra

--- Assets/Scripts/PajaritoVolar.cs
+++ Assets/Scripts/PajaritoVolar.cs
@@ -7,6 +7,10 @@ public class PajaritoVolar : MonoBehaviour
 {
     bool Activo;
 
+    public GameObject Pajaro;
+    public AnimationClip Volar;
+    private Animation AnimacionVolar;
+
     float Posx;
     float Posy;
     float t1;
@@ -15,70 +19,33 @@ public class PajaritoVolar : MonoBehaviour
     private void Start()
     {
         Activo = false;
+        AnimacionVolar = Pajaro.AddComponent<Animation>();
+        AnimacionVolar.AddClip(Volar, "volar");
+        Volar.legacy = true;
     }
 
     private void Update()
     {
-        if(Activo)
+        if (Activo)
         {
             t1 += .4f * Time.deltaTime;
             t2 += .1f * Time.deltaTime;
             this.transform.position = new Vector3(Mathf.Lerp(Posx, Posx + 20, t1), Mathf.Lerp(Posy, Posy + 20, t2), this.transform.position.z);
+            AnimacionVolar.Play("volar");
         }
     }
 
     void OnTriggerEnter(Collider collision)
     {
+        
         if (collision.gameObject.tag == "Player")
         {
             Posx = this.transform.position.x;
             Posy = this.transform.position.y;
             Activo = true;
+            Debug.Log("entramos");
         }
     }
 }
 
 
-/*
-bool Vuela;
-float Posx;
-float Posy;
-
-private void Start()
-{
-    Vuela = false;
-}
-
-private void Update()
-{
-    if (Vuela == true)
-    {
-        this.transform.position = new Vector3(Posx, Posy, this.transform.position.z);
-    }
-}
-
-void OnTriggerEnter(Collider collision)
-{
-    if (collision.gameObject.tag == "Player")
-    {
-        Posx = this.transform.position.x;
-        Posy = this.transform.position.y;
-        Vuela = true;
-        Volar();
-    }
-}
-
-void Volar()
-{
-    Debug.Log(Posx + "," + Posy);
-    for (float i = 0; i <= 1000; i += .01f)
-    {
-        Posx += .0001f;
-        Posy += .0001f;
-        if (i >= 1000)
-        {
-            Vuela = false;
-            Debug.Log(Vuela);
-        }
-    }
-}*/
