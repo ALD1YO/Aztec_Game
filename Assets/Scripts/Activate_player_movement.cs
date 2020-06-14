407f14e8eacb971d618f0864fd468061b05ee34b
ald1yo <crespoaldo29@gmail.com>
2020-06-13 22:19:05 -0500

    Se pone la base para la cámara del menú principal

---
+++ Assets/Scripts/Activate_player_movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activate_player_movement : MonoBehaviour
+{
+
+    public ShapeShift _shapeshift;
+    public P_Anim _p_anim;
+    public Animator _Menu_Camara_Animacion;
+    public ThirdPerson _ThirdPerson;
+    private bool JuegoEmpezado = false;
+    public GameObject Camara_Principal;
+    public GameObject Camara_Menu;
+    public GameObject Player_Canvas;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        //_shapeshift = _shapeshift.GetComponent<ShapeShift>();
+        //_Menu_Camara_Animacion = _Menu_Camara_Animacion.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Con esto se quita el menu e inicia el juego
+        if (Input.GetKeyDown(KeyCode.Space) && !JuegoEmpezado)
+        {
+            JuegoEmpezado = true;
+
+            //Activa el movimiento de la cámara e incia un countdown para activar los controles
+            _Menu_Camara_Animacion.Play("Cam_Animation", -1, 0.0f);
+            StartCoroutine(Activar_Movimiento());
+        }
+
+    }
+
+     IEnumerator Activar_Movimiento()
+    {
+        //Codigo de empezar animacion de cámara
+
+        yield return new WaitForSeconds(4.0f);
+        print("Se inicia el juego");
+        _shapeshift.enabled = true;
+        _p_anim.enabled = true;
+        _ThirdPerson.enabled = true;
+        Player_Canvas.SetActive(true);
+        //Camara_Principal.SetActive(true);
+        //Camara_Menu.SetActive(false);
+    }
+}
--- Assets/Scripts/Player_Scripts/ThirdPerson.cs
+++ Assets/Scripts/Player_Scripts/ThirdPerson.cs
@@ -7,6 +7,8 @@ public class ThirdPerson : MonoBehaviour
     public Transform playerCam, character, centerPoint;
     public P_Anim moveAnim;
 
+    public float centerPoint_y_personalAdjustment;
+
     private float mouseX, mouseY;
     public float mouseSensitivity = 10f;
     public float mouseYPosition = 1f;
@@ -105,7 +107,7 @@ public class ThirdPerson : MonoBehaviour
         Vector3 movement = new Vector3(moveLR, -gravityForce, moveFB);
         movement = character.rotation * movement;
         character.GetComponent<CharacterController>().Move(movement * Time.deltaTime);
-        centerPoint.position = new Vector3(character.position.x, character.position.y + mouseYPosition, character.position.z);
+        centerPoint.position = new Vector3(character.position.x, character.position.y- centerPoint_y_personalAdjustment + mouseYPosition, character.position.z);
 
         if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0 
             || Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0)
@@ -119,8 +121,8 @@ public class ThirdPerson : MonoBehaviour
     }
     void centraCamara()
     {
-        ShapeShift shapeShift = ShapeShift_script.GetComponent<ShapeShift>();
-        playerCam.transform.localPosition = new Vector3(0, shapeShift.posicion_camara, zoom);
+        //ShapeShift shapeShift = ShapeShift_script.GetComponent<ShapeShift>();
+        //playerCam.transform.localPosition = new Vector3(0, shapeShift.posicion_camara, zoom);
         mouseY = Mathf.Clamp(mouseY, -20f, 30f);
         playerCam.LookAt(centerPoint);
         centerPoint.localRotation = Quaternion.Euler(mouseY, mouseX, 0);
